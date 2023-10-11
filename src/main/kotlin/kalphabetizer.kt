09683f88a4b5bb982465f80de0535dc2d3ae42f2
Onkornix <101086445+Onkornix@users.noreply.github.com>
2023-10-11 14:27:10 -0700

    finished kalphabetizer

--- src/main/kotlin/kalphabetizer.kt
+++ src/main/kotlin/kalphabetizer.kt
@@ -1,6 +1,4 @@
-
-//val input:String = readln()
-val input:String = "hello world"
+//global variables
 val inputList: MutableList<Char> = mutableListOf()
 val StringIntegerMap: Map<Char, Int> = mapOf(
     'a' to 0, 'b' to 1, 'c' to 2, 'd' to 3, 'e' to 4, 'f' to 5, 'g' to 6,
@@ -9,69 +7,81 @@ val StringIntegerMap: Map<Char, Int> = mapOf(
     'v' to 21, 'w' to 22, 'x' to 23, 'y' to 24, 'z' to 25,
 )
 
+fun doWordSorting(){
+
+    //some vars i guess
+    //val wordMap: MutableMap<Int, String> = mutableMapOf()
+    val wordList: MutableList<String> = mutableListOf()
+    val wordBuild: MutableList<Char> = mutableListOf() // base for dissecting sentence and building individual words
+    var iteration = 1
+
+    //val charWordMap: MutableMap<Char, String> = mutableMapOf()
+    for (l in inputList){
+
+        // conditions
+        when {
+            //when space is encountered
+            (l !in StringIntegerMap) -> {
+                wordList.add(wordBuild.joinToString(""))
+                wordBuild.clear()
+                iteration++
+            }
+
+            //when last letter is reached
+            (iteration == inputList.size) -> {
+                wordBuild.add(l)
+                wordList.add(wordBuild.joinToString(""))
+                wordBuild.clear()
+
+
+            }
+
+            //for every other letter
+            else -> {
+                wordBuild.add(l)
+                iteration++
+            }
+        }
+    }
+    wordList.sort()
+    println(wordList.joinToString(" "))
+
+
+}
+
+
+fun doLetterSorting(){
+
+    // breaking up, changing to Int, replacing
+    val charIntList: MutableList<Int> = mutableListOf() // character to integer list init
+    for (c in inputList){ // taking Characters
+        charIntList.add(StringIntegerMap[c]!!)
+    }
+
+    //sorting (kinda messed up how easy it is lol. I don't even need to convert it to Int)
+    charIntList.sort()
+
+
+    //getting Character keys from Map using Integer values
+    val lastStringList: MutableList<String> = mutableListOf()
+    for (i in charIntList){ // taking integers
+        val filterValues = StringIntegerMap.filterValues { it == i }
+        lastStringList.add(filterValues.keys.joinToString())
+
+    }
+    //finally done.
+    print(lastStringList.joinToString(""))
+
+}
+
 fun main(){
     //println(StringIntegerTable.indexOf("a"))
     println("Enter a sentence or word and it will get alphabetized")
+    val input:String = readln()
     for (l in input){
-        inputList.add(l)
+        inputList.add(l) //creating list of characters
     }
-    if (' ' in inputList) { // start sentence sorting
-
-        //some vars i guess
-        val wordMap: MutableMap<String, Int> = mutableMapOf()
-        val wordBuild: MutableList<Char> = mutableListOf() // base for dissecting sentence and building individual words
-        val wordList: MutableList<String> = mutableListOf() // base for adding words to list
-        var iteration: Int = 0
-
-        //val charWordMap: MutableMap<Char, String> = mutableMapOf()
-        for (l in inputList){
-            // conditions
-
-            when {
-                (l == ' ') -> { //when space is encountered
-                    wordMap[wordBuild.joinToString("")] = iteration // concat wordBuild and add to wordList
-                    wordBuild.clear()
-                    iteration++
-                }
-                (l == inputList.last()) -> { //when last letter is reached
-                    wordBuild.add(l)
-                    wordMap[wordBuild.joinToString("")] = iteration // concat wordBuild and add to wordList
-                    wordBuild.clear()
-                }
-                else -> wordBuild.add(l) //for every other letter
-            }
-        }
-        println(wordMap)
-
-    }
-
-
-
-
-    // Sorting a single word.
-    else {
-
-        // breaking up, changing to Int, replacing
-        val charIntList: MutableList<Int> = mutableListOf() // character to integer list init
-        for (c in inputList){ // taking Characters
-            charIntList.add(StringIntegerMap[c]!!) //
-        }
-
-
-        //sorting (kinda messed up how easy it is lol. I don't even need to convert it to Int)
-        charIntList.sort()
-
-
-        //getting Character keys from Map using Integer values
-        val lastStringList: MutableList<String> = mutableListOf()
-        for (i in charIntList){ // taking integers
-            //intCharList.add()
-            val filterValues = StringIntegerMap.filterValues { it == i }
-            lastStringList.add(filterValues.keys.joinToString())
-
-        }
-        //finally done.
-        print(lastStringList.joinToString(""))
-
-    }
+    if (' ' in inputList) {
+        doWordSorting() // start sentence sorting
+    } else doLetterSorting() // Sorting a single word
 }
